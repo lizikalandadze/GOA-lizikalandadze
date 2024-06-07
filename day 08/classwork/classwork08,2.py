76e80242938153374f1c37aa9238bccf3895553a
lizikalandadze <lizikalandadze.z09@gmail.com>
2024-06-07 21:35:28 +0400

    Add files via upload

---
+++ day 08/classwork/classwork08,2.py
@@ -0,0 +1,4 @@
+price =10
+discount =2
+jami =12
+print(price, discount, jami)
---
+++ day 08/homework/lesson08.py
@@ -0,0 +1,3 @@
+num1 = input("enter your first number: ")
+num2 =input("enter your second number:")
+print(num1 + num2)
