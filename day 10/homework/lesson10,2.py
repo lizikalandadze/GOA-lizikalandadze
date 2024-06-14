2953371610e3e3e1c065f662b871053ec961b3d2
lizikalandadze <lizikalandadze.z09@gmail.com>
2024-06-14 15:09:23 +0400

    Add files via upload

---
+++ day 10/homework/lesson10,2.py
@@ -0,0 +1,4 @@
+name = input("Enter your name: ")
+lastname = input("Enter your lastname:")
+age = input("Enter your age:")
+print("my name is name", name, "my lastname is", lastname, "my age is", age )
